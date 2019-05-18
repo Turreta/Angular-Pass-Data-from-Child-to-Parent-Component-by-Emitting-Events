752a2efefc63c4048e6eb86291edb5856cb75831
Turreta.com <Turreta@users.noreply.github.com>
2019-05-18 22:55:23 +0800

    Add files via upload

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Output("itemNameFromChild") itemNameEmitter: EventEmitter<string> = new EventEmitter<string>();
+
+  itemName = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddItem() {
+    this.itemNameEmitter.emit(this.itemName);
+    this.itemName = '';
+  }
+
+}
